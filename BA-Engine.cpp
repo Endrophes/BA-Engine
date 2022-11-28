378384b77db7d853449b0ea4680fb1ddf15c32fd
Endrophes <Endrophes@users.noreply.github.com>
2022-11-28 15:54:36 -0500

    Update BA-Engine.cpp
    Added some more specialization to main function, for future expansion.

--- BA-Engine.cpp
+++ BA-Engine.cpp
@@ -1,8 +1,31 @@
 
+#include <iostream>
 
-// BA-Engine.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+/// <summary>
+/// The start of engine
+/// </summary>
+/// <returns>Error Codes</returns>
 int main()
 {
+	bool run = true;
+
+	std::cout << "Type \"exit\" to end the program" << std::endl;
+
+	while (run)
+	{
+		std::string userinput;
+		std::cin >> userinput;
+
+		if (userinput == "Exit" || userinput == "exit")
+		{
+			std::cout << "Program terminated by user." << std::endl;
+			run = false;
+		}
+		else
+		{
+			std::cout << "unknown command" << std::endl;
+		}
+	}
+
 	return 0;
 }
