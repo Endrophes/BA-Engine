4d839bc218a81f87164fe0a450cc0376a2c9ccc3
Endrophes <Endrophes@users.noreply.github.com>
2022-11-28 11:53:36 -0500

    Update ComponentSystem.h

--- Systems/ComponentSystem.h
+++ Systems/ComponentSystem.h
@@ -1,1 +1,35 @@
-#pragma once
+
+#ifndef ComponentSystem
+#define ComponentSystem
+
+#include "EntitySystem.h"
+#include "../BaseClass/IComponent.h"
+#include <map>
+#include <vector>
+
+#define kInvalid_Comp 00000
+
+//Controller and holder for all components and their entities
+class ComponentSystem
+{
+	/// <summary>
+	/// Pair's components to entities
+	/// </summary>
+	std::map<EntityId, std::vector<IComponent>> EntitesComponentTree;
+
+public:
+
+	/// <summary>
+	/// Create a new instance of a Component 
+	/// </summary>
+	/// <param name="entId">Parent Entity</param>
+	static void addComponent(EntityId entId);
+
+	/// <summary>
+	/// Shutdown an instance of a Component
+	/// </summary>
+	/// <param name="entId">Parent Entity</param>
+	static void removeComponent(EntityId entId);
+};
+
+#endif //ComponentSystem
