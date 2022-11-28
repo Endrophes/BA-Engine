59d0cf495dcfb0d552c45c1cbaddbe5f6d29b877
Endrophes <Endrophes@users.noreply.github.com>
2022-11-28 11:52:24 -0500

    Create IComponent.h

---
+++ BaseClass/IComponent.h
@@ -0,0 +1,28 @@
+#ifndef I_COMPONENT_H
+#define I_COMPONENT_H
+
+//Define
+class IComponent
+{
+public:
+
+	/// <summary>
+	/// Grab or initialize parts of the component before start up
+	/// </summary>
+	/// <param name="entId">Id of the parent entity</param>
+	virtual void setup(EntityId entId);
+
+	/// <summary>
+	/// Ran every frame to make changes to logic
+	/// </summary>
+	/// <param name="entId">Id of the parent entity</param>
+	virtual void update(EntityId entId);
+
+	/// <summary>
+	/// Close out and clear data when removing the component
+	/// </summary>
+	/// <param name="entId">Id of the parent entity</param>
+	virtual void shutdown(EntityId entId);
+};
+
+#endif //I_COMPONENT_H
