d3b00926c313e1c3c234e9b0351fc745851c8694
Endrophes <Endrophes@users.noreply.github.com>
2022-11-30 16:41:22 -0500

    Update IComponent.h

--- src/BaseClass/IComponent.h
+++ src/BaseClass/IComponent.h
@@ -17,19 +17,19 @@ namespace BA_Engine
 		/// Grab or initialize parts of the component before start up
 		/// </summary>
 		/// <param name="entId">Id of the parent entity</param>
-		virtual void setup(EntityId entId);
+		virtual void setup(EntityId entId) = 0;
 
 		/// <summary>
 		/// Ran every frame to make changes to logic
 		/// </summary>
 		/// <param name="entId">Id of the parent entity</param>
-		virtual void update(EntityId entId);
+		virtual void update(EntityId entId) = 0;
 
 		/// <summary>
 		/// Close out and clear data when removing the component
 		/// </summary>
 		/// <param name="entId">Id of the parent entity</param>
-		virtual void shutdown(EntityId entId);
+		virtual void shutdown(EntityId entId) = 0;
 	};
 
 }
