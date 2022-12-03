e07af57214ed397ddc2f0875cc5f2df351e3eb00
Endrophes <Endrophes@users.noreply.github.com>
2022-12-02 23:03:50 -0500

    Update IComponent.h
    Figured out the more data driven approach

--- src/BaseClass/IComponent.h
+++ src/BaseClass/IComponent.h
@@ -18,17 +18,17 @@ namespace BA_Engine
 		/// <summary>
 		/// Grab or initialize parts of the component before start up
 		/// </summary>
-		virtual void setup(CompData data) const = 0;
+		//virtual void setup() const = 0;
 
 		/// <summary>
 		/// Ran every frame to make changes to logic
 		/// </summary>
-		virtual void update(CompData data) const = 0;
+		//virtual void update() const = 0;
 
 		/// <summary>
 		/// Close out and clear data when removing the component
 		/// </summary>
-		virtual void shutdown(CompData data) const = 0;
+		//virtual void shutdown() const = 0;
 	};
 
 }
