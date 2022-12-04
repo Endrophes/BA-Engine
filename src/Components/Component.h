ba259d5dabf296640d19ae291a567b04860872a4
Endrophes <Endrophes@users.noreply.github.com>
2022-12-04 14:35:52 -0500

    Update Component.h
    Started moving all the components to the one file.
    Getting lazy on this as I want to get this done.

--- src/Components/Component.h
+++ src/Components/Component.h
@@ -6,14 +6,113 @@
 
 // Started getting really lazy and now I'm storing all my components in here
 
+// TODO: make into structs?
+
 namespace BA_Engine
 {
 
+    /// <summary>
+    /// Life value of the Entity
+    /// </summary>
+    class Health : public IComponent
+    {
+    public:
 
-	static void registorComponents( )
-	{
+        float mHealth;
 
-	}
+        //Hit by
+        std::vector< std::pair< std::string, float > > mHitBy;
+
+        Health() = default;
+        Health(const Health&) = default;
+        Health(float pHealth)
+            : mHealth(pHealth)
+        {}
+
+    };
+
+    // ## Weapons ##
+
+    /// <summary>
+    /// A Romulan heavy weapon
+    /// </summary>
+    class Disruptor : public virtual IComponent
+    {
+    public:
+
+        float mMaxRange;
+        float mDamage;
+
+        Disruptor() = default;
+        Disruptor(const Disruptor&) = default;
+        Disruptor(float pMaxRange, float pDamage)
+            : mMaxRange(pMaxRange)
+            , mDamage(pDamage)
+        {}
+    };
+
+    /// <summary>
+    /// Federation long range energy weapon
+    /// </summary>
+    class Phaser : public virtual IComponent
+    {
+    public:
+
+        float mMaxRange;
+        float mDamage;
+        bool mOnline;
+
+        Phaser() = default;
+        Phaser(const Phaser&) = default;
+        Phaser(float pMaxRange, float pDamage, bool pOnline)
+            : mMaxRange(pMaxRange)
+            , mDamage(pDamage)
+            , mOnline(pOnline)
+        {}
+    };
+
+    /// <summary>
+    /// Federation short range heavy weapon
+    /// </summary>
+    class PhotonTorpedo : public virtual IComponent
+    {
+    public:
+
+        float mMaxRange;
+        float mDamage;
+        bool mOnline;
+
+        PhotonTorpedo() = default;
+        PhotonTorpedo(const PhotonTorpedo&) = default;
+        PhotonTorpedo(float pMaxRange, float pDamage, bool pOnline)
+            : mMaxRange(pMaxRange)
+            , mDamage(pDamage)
+            , mOnline(pOnline)
+        {}
+    };
+
+    // ## Sub-light thrusters ##
+
+    /// <summary>
+    /// A Standard drive engine for starship's
+    /// </summary>
+    class ImpulseEngines : public virtual IComponent
+    {
+    public:
+
+        float mMaxSpeed;
+        float mSpeed;
+        bool mOnline;
+
+        ImpulseEngines() = default;
+        ImpulseEngines(const ImpulseEngines&) = default;
+        ImpulseEngines(float pMaxSpeed, float pSpeed, bool pOnline)
+            : mMaxSpeed(pMaxSpeed)
+            , mSpeed(pSpeed)
+            , mOnline(pOnline)
+        {}
+    };
+
 }
 
 #endif //COMPONENT_H
