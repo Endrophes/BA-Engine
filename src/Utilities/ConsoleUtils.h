7c98459e343aef7747ee399aea33866bfb870f4f
Endrophes <Endrophes@users.noreply.github.com>
2022-12-04 15:36:29 -0500

    Update ConsoleUtils.h
    Overloaded printAndGetOption so that I can get a fixed int value back for a given option.

--- src/Utilities/ConsoleUtils.h
+++ src/Utilities/ConsoleUtils.h
@@ -41,6 +41,33 @@ namespace BA_Engine
 
 			return optionPicked;
 		}
+
+		/// <summary>
+		/// Takes in an option vector, but returns a given int value paired with it
+		/// Helpful for dynamic console values
+		/// </summary>
+		static int printAndGetOption(std::string mainMsg, std::vector<std::pair<std::string, int>> options)
+		{
+			printMessage(mainMsg);
+
+			int opNum = 1;
+			for (std::pair<std::string, int> option : options)
+			{
+				printMessage((std::to_string(opNum) + ". " + option.first).c_str());
+				opNum++;
+			}
+
+			int optionPicked = 0;
+			std::cin >> optionPicked;
+
+			if (optionPicked == 0 || optionPicked > opNum)
+			{
+				printMessage("Invalid option");
+				return printAndGetOption(mainMsg, options); //Recursion for the win?
+			}
+
+			return options[optionPicked].second;
+		}
 	};
 
 }
