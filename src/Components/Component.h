6353d53e610a68d0f8a51027c01d06a45ea1c99c
Endrophes <Endrophes@users.noreply.github.com>
2022-12-03 23:39:35 -0500

    Create Component.h
    Getting lazy and will be adding all the components into here

---
+++ src/Components/Component.h
@@ -0,0 +1,19 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include "../PrecompiledHeader.h"
+#include "../BaseClass/IComponent.h"
+
+// Started getting really lazy and now I'm storing all my components in here
+
+namespace BA_Engine
+{
+
+
+	static void registorComponents( )
+	{
+
+	}
+}
+
+#endif //COMPONENT_H
