84d253f8d69d7a36110be18f1d836877ddf614b6
Endrophes <Endrophes@users.noreply.github.com>
2022-12-03 21:58:41 -0500

    Create GameController.h
    To get most of the game code out of main, I just have this function to star and run the game.

---
+++ src/BaseClass/GameController.h
@@ -0,0 +1,43 @@
+#ifndef GAME_CONTROLLER_H
+#define GAME_CONTROLLER_H
+
+//#includes
+
+namespace BA_Engine
+{
+
+    /// <summary>
+    /// Manages the state of a game
+    /// </summary>
+    class GameController
+    {
+    private:
+
+        bool isRunning;
+
+    public:
+
+        /// <summary>
+        /// Put all the pieces in place
+        /// </summary>
+        virtual void setup() = 0;
+
+        /// <summary>
+        /// Setup the game board
+        /// </summary>
+        virtual void start() = 0;
+
+        /// <summary>
+        /// Cycle the game
+        /// </summary>
+        virtual void update(const float pElapsedTime) = 0;
+
+        /// <summary>
+        /// Clear the game board
+        /// </summary>
+        virtual void shutdown() = 0;
+    };
+
+}
+
+#endif //GAME_CONTROLLER_H
