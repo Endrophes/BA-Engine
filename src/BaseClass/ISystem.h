65e3be93e8ada1db5ecbec7341b8ccfb0a66b86f
Endrophes <Endrophes@users.noreply.github.com>
2022-12-03 21:58:04 -0500

    Update ISystem.h

--- src/BaseClass/ISystem.h
+++ src/BaseClass/ISystem.h
@@ -19,7 +19,7 @@ namespace BA_Engine
         /// <summary>
         /// Perform operations for the given system
         /// </summary>
-        virtual void run(const float elapsedTime) = 0;
+        virtual void run(const float pElapsedTime) = 0;
 
     };
 
