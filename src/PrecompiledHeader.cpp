7684d3fcd1ff9a665b877d288455cfaac72a40ce
Endrophes <Endrophes@users.noreply.github.com>
2022-12-05 10:36:34 -0500

    Adding Copyright notice to all files

--- src/PrecompiledHeader.cpp
+++ src/PrecompiledHeader.cpp
@@ -1,1 +1,10 @@
+/*******************************************************
+ * Copyright (C) 2022 Bastien A. Auxer <auxerbastien@gmail.com>
+ *
+ * This file is part of BA-Engine project.
+ *
+ * BA-Engine project can not be copied and/or distributed without the express
+ * permission of Bastien A. Auxer
+ *******************************************************/
+
 #include "PrecompiledHeader.h"
