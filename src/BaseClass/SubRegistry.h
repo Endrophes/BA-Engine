c8528231d1488406fffcc46226dfe99f76905220
Endrophes <Endrophes@users.noreply.github.com>
2023-01-31 14:00:22 -0500

    Create SubRegistry.h
    Started working out a smaller class that will manage individual component storage, rather than the one big Registry class.

---
+++ src/BaseClass/SubRegistry.h
@@ -0,0 +1,17 @@
+#ifndef SUB_REGISTRY_SYSTEM_H
+#define SUB_REGISTRY_SYSTEM_H
+
+#include "../PrecompiledHeader.h"
+
+namespace BA_Engine
+{
+
+	template<class T>
+	class SubRegistry
+	{
+		//std::vector<T> 
+	};
+
+}
+
+#endif //SUB_REGISTRY_SYSTEM_H
